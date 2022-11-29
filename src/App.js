b501d6c2a4cd97def09f8cf88386b8b4d8fc9c95
ashleydelic <ashleycohzer96@gmail.com>
2022-11-29 13:46:56 -0500

    containers, location, temp and description

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import axios from 'axios';
 function App() {
   // const url='https://api.openweathermap.org/data/2.5/weather?q=lexington,ky,usa&appid=5bc10b7aed41cf1f5c7aa583cd837f33';
   return (
-    <div className="App">
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Lexington, KY</p>
+          </div>
+          <div className='temp'>
+            <h1>65 °F</h1>
+          </div>
+          <div className='description'>
+            
+          </div>
+        </div>
+        <div className='bottom'>
+
+        </div>
+      </div>
    
     </div>
   );
