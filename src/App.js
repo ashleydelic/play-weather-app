e07dca23b8532558f88dc6c86989596a51fed1d5
ashleydelic <ashleycohzer96@gmail.com>
2022-11-29 13:12:13 -0500

    api upload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
-  const url='https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=5bc10b7aed41cf1f5c7aa583cd837f33';
+  // const url='https://api.openweathermap.org/data/2.5/weather?q=lexington,ky,usa&appid=5bc10b7aed41cf1f5c7aa583cd837f33';
   return (
     <div className="App">
    
