9f7b6e94037e14c78d9633d6a4e7a91e8df5a03a
ashleydelic <ashleycohzer96@gmail.com>
2022-11-29 12:18:17 -0500

    got API from openweathermap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const url='https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=5bc10b7aed41cf1f5c7aa583cd837f33';
   return (
     <div className="App">
    
