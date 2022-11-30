420cd9ee837a64a01e153797f1e01d14ec654124
ashleydelic <ashleycohzer96@gmail.com>
2022-11-30 18:09:32 -0500

    more fetch api data code

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ function App() {
     if (event.key === "Enter") {
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
         response => response.json
+      ).then(
+        data => {
+          setWeatherData(data)
+        }
       )
 
     }
