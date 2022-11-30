8051160f14f0801cf81b48225a52d75851c943ef
ashleydelic <ashleycohzer96@gmail.com>
2022-11-30 11:29:09 -0500

    api key and usestate variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 
+
+
+
 function App() {
 
-  c
+  const apiKey = '9384c66feb1bae1fbcdcb63411c94cf7';
+  const [weatherData, setWeatherData] = useState;
   
   const url = 'https://api.openweathermap.org/data/2.5/weather?q=${}&appid=5bc10b7aed41cf1f5c7aa583cd837f33';
 
@@ -12,17 +16,9 @@ function App() {
   return (
     <div className="app">
       <div className='search'>
-        <input 
-        value={location}
-        onChange={event => setLocation(event.target.value)}
+        <input
         placeholder='Enter location'
-        onKeyPress={searchLocation}
         type="text"/>
-        <input 
-        value='submit'
-        type='Submit'
-        
-        
       </div>
       <div className='container'>
         <div className='top'>
