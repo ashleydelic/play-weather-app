7c6fcbab1a31348c5981e9695f08aa49d2c00717
ashleydelic <ashleycohzer96@gmail.com>
2022-11-29 20:13:23 -0500

    bottom margin adjustment

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="app">
-      <div className='input'>
+      <div className='search'>
         <input 
         value={location}
         onChange={event => setLocation(event.target.value)}
